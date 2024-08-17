19425a59b076c9a4ae4162a4b77d59653cc10361
Kamran-Zeynalov <kamraneze@code.edu.az>
2024-08-17 17:25:26 +0400

    IM Garbarage collector

--- Final-project/obj/Debug/net6.0/Final-project.AssemblyInfo.cs
+++ Final-project/obj/Debug/net6.0/Final-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7680927272c10a7c9ae5472450f6cfacf6980be9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1756cd680c4f342048e0d49d58519f4279e6d8b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
